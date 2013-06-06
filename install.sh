38d15a0a0f2f33f8b4be71b5184ed1e7a604afea
Marc Delagrammatikas <delagrammatikas@gmail.com>
2013-06-05 23:28:38 -0700

    Remove copy

--- install.sh
+++ install.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
-cp "$PWD/vim" "$HOME/.vim"
 ln -s "$PWD/vim" "$HOME/.vim"
-cp "$PWD/vim/vimrc" "$HOME/.vimrc"
 ln -s "$PWD/vim/vimrc" "$HOME/.vimrc"
-cp "$PWD/vim/gvimrc" "$HOME/.gvimrc"
 ln -s "$PWD/vim/gvimrc" "$HOME/.gvimrc"
 
-cp "$PWD/zsh" "$HOME/.zsh"
 ln -s "$PWD/zsh" "$HOME/.zsh"
-cp "$PWD/zshrc" "$HOME/.zshrc"
 ln -s "$PWD/zshrc" "$HOME/.zshrc"
 
-cp "$PWD/gitconfig" "$HOME/.gitconfig"
 ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
-
-cp "$PWD/gitignore" "$HOME/.gitignore"
 ln -s "$PWD/gitignore" "$HOME/.gitignore"
-
-cp "$PWD/rspec" "$HOME/.rspec"
 ln -s "$PWD/rspec" "$HOME/.rspec"
 
 git clone git@git.wincent.com:command-t.git "$HOME/.vim/bundle"
