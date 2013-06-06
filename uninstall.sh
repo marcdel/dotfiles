d196b97ec858e37d4253954c1031714b780d79ef
Marc Delagrammatikas <delagrammatikas@gmail.com>
2013-06-05 23:45:01 -0700

    Add uninstall file

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm -r "$HOME/.vim"
+rm -r "$HOME/.vimrc"
+rm -r "$HOME/.gvimrc"
+
+rm -r "$HOME/.zsh"
+rm -r "$HOME/.zshrc"
+
+rm -r "$HOME/.gitconfig"
+rm -r "$HOME/.gitignore"
+rm -r "$HOME/.rspec"
