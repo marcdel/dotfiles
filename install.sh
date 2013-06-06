ea30f8ea5ac7f9391a13157cb3bb303a761beb94
Marc Delagrammatikas <delagrammatikas@gmail.com>
2013-06-05 23:25:32 -0700

    Change ls to ln

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 cp "$PWD/vim" "$HOME/.vim"
-ls -s "$PWD/vim" "$HOME/.vim"
+ln -s "$PWD/vim" "$HOME/.vim"
 cp "$PWD/vim/vimrc" "$HOME/.vimrc"
-ls -s "$PWD/vim/vimrc" "$HOME/.vimrc"
+ln -s "$PWD/vim/vimrc" "$HOME/.vimrc"
 cp "$PWD/vim/gvimrc" "$HOME/.gvimrc"
-ls -s "$PWD/vim/gvimrc" "$HOME/.gvimrc"
+ln -s "$PWD/vim/gvimrc" "$HOME/.gvimrc"
 
 cp "$PWD/zsh" "$HOME/.zsh"
-ls -s "$PWD/zsh" "$HOME/.zsh"
+ln -s "$PWD/zsh" "$HOME/.zsh"
 cp "$PWD/zshrc" "$HOME/.zshrc"
-ls -s "$PWD/zshrc" "$HOME/.zshrc"
+ln -s "$PWD/zshrc" "$HOME/.zshrc"
 
 cp "$PWD/gitconfig" "$HOME/.gitconfig"
-ls -s "$PWD/gitconfig" "$HOME/.gitconfig"
+ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
 
 cp "$PWD/gitignore" "$HOME/.gitignore"
-ls -s "$PWD/gitignore" "$HOME/.gitignore"
+ln -s "$PWD/gitignore" "$HOME/.gitignore"
 
 cp "$PWD/rspec" "$HOME/.rspec"
-ls -s "$PWD/rspec" "$HOME/.rspec"
+ln -s "$PWD/rspec" "$HOME/.rspec"
 
 git clone git@git.wincent.com:command-t.git "$HOME/.vim/bundle"
 git clone git@github.com:altercation/vim-colors-solarized.git "$HOME/.vim/bundle"
