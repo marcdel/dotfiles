eb800a4ba918d389aec61b9dbedce131e707922b
Marc Delagrammatikas <delagrammatikas@gmail.com>
2013-06-10 01:29:06 -0700

    Cloning the vundle repo somehow causes a symlink loop.
    Trying to clone the repo from the bash script fails.
    Somehow this causes a symlink to be created in dotfiles/vim
    which points to ~/.vim which in turn points back to dotfiles/vim.
    They poop back and forth forever. I guess I just have to do that step
    manually until i figure out the issue.

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
 ln -s "$PWD/gitignore" "$HOME/.gitignore"
 ln -s "$PWD/rspec" "$HOME/.rspec"
 
+# Something about cloning the git repo or cloning and failing causes
+# a weird infinite loop of symlinks between dotfiles/vim and ~/.vim
+
 #cd "$Home/.vim/bundle/"
 #git clone git@github.com:gmarik/vundle.git
-
-git clone git@github.com:gmarik/vundle.git "$Home/.vim/bundle/vundle"
-
-vim +BundleInstall +qall
+#vim +BundleInstall +qall
