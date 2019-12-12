6f81062932dde2f784bb3252d2569cdb8f63951c
Marc Delagrammatikas <delagrammatikas@gmail.com>
2019-12-12 13:11:06 -0800

    Setup packages using minpac

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -s "$PWD/vim" "$HOME/.vim"
+ln -s "$PWD/vimrc" "$HOME/.vimrc"
+
+ln -s "$PWD/zshrc" "$HOME/.zshrc"
+
+ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
+ln -s "$PWD/gitignore" "$HOME/.gitignore"
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -r "$HOME/.vim"
+rm -r "$HOME/.vimrc"
+
+rm -r "$HOME/.zshrc"
+
+rm -r "$HOME/.gitconfig"
+rm -r "$HOME/.gitignore"
